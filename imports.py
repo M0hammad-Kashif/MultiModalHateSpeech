7a2967363735c9ecac538c471fecb80b6195a580
Mohammad Kashif <iamkashif20@gmail.com>
2023-06-23 10:54:45 -0400

    added all imports

---
+++ imports.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import transformers
+from transformers import AutoModel, BertTokenizerFast
+from transformers import BertTokenizerFast as BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+import pytesseract
+from glob import glob
+from random import shuffle
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import time
+import os
+from tqdm import tqdm
+import torch.optim as optim
+from unidecode import unidecode
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+import torch.nn.functional as F
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
